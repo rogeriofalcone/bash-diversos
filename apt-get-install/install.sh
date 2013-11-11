b33ee4510a3b4f211f12b20c609947640ad4957d
Paulo Roberto <paulo@notebook-hp.(none)>
2013-11-10 23:39:56 -0200

    Arquivo texto com todos repositorio que serão usados.

--- apt-get-install/install.sh
+++ apt-get-install/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Script para instalação de pacotes padrões para meu ambiente de trabalho.
 
+# Adicionando os repositórios
+for line in $(cat repositories.txt)
+	do apt-add-repository "$line" -y
+done
+
+# Instalando os pacotes
 for line in $(cat packages.txt)
     do apt-get install "$line"  -y
 done 
